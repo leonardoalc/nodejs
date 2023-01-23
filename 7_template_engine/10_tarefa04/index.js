e6c4620a6c32df94dfeb5324afbd83e5ab4d4b78
LeL-nardo <sandroleonardoads@gmail.com>
2023-01-23 14:20:35 -0300

    tarefa 04, template engine

--- 7_template_engine/10_tarefa04/index.js
+++ 7_template_engine/10_tarefa04/index.js
@@ -1,4 +1,43 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+const port = 5000
 
 const app = express()
+
+const products = [
+    {
+        id: 0,
+        name: "camisa",
+        price: 100
+    },
+    {
+        id: 1,
+        name: "short",
+        price: 60
+    },
+    {
+        id: 2,
+        name: "tênis",
+        price: 150
+    }
+]
+
+app.use(express.static("public"))
+
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+app.get("/products/:id", (req, res) =>  {
+    const product = products[req.params.id]
+
+    res.render("product", {product})
+})
+app.get("/", (req, res) => {
+
+    res.render("home", {products})
+})
+
+app.listen(port, () => {
+
+    console.log(`rodando na porta: ${port}`)
+})
