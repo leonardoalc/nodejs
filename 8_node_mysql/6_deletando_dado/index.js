fec856400cc74f10f3ad7d01d934fb59bb752e76
LeL-nardo <sandroleonardoads@gmail.com>
2023-01-26 22:09:52 -0300

    node sql finalizado

--- 8_node_mysql/6_deletando_dado/index.js
+++ 8_node_mysql/6_deletando_dado/index.js
@@ -4,6 +4,7 @@ const mysql = require("mysql")
 
 const app = express()
 
+// transformando dados recebidos em json
 app.use(
     express.urlencoded({
         extended: true
@@ -12,15 +13,21 @@ app.use(
 
 app.use(express.json())
 
+// setanndo handlebars como template engine
 app.engine("handlebars", exphbs.engine())
 app.set("view engine", "handlebars")
 
+// setado static de css
 app.use(express.static("public"))
 
+// página principal
 app.get("/", (req, res) => {
     res.render("home")
 })
 
+// criação e postagem de livro no banco de dados
+// a action do formulário será  a mesma action do post
+// ao final, redirecione o usuário para a página pricipal.
 app.post("/books/insertbook", (req, res) => {
     const title = req.body.title
     const pages = req.body.pagesqty
@@ -36,6 +43,7 @@ app.post("/books/insertbook", (req, res) => {
     })
 })
 
+// conseguindo dados do banco de dados
 app.get("/books", (req, res) => {
 
     const sql = "SELECT * FROM books"
@@ -50,6 +58,8 @@ app.get("/books", (req, res) => {
         res.render("books", {books})
     })
 })
+
+// selecionando um dado específico com o id do banco de dados
 app.get("/books/:id", (req, res) => {
     const id = req.params.id
 
@@ -66,6 +76,44 @@ app.get("/books/:id", (req, res) => {
         res.render("book", {book})
     })
 })
+
+// criando página de edição dados
+app.get("/books/edit/:id", (req, res) => {
+
+    const id = req.params.id
+
+    const sql = `SELECT * FROM books WHERE idbooks = ${id}`
+
+    conn.query(sql, function(err, data) {
+        if(err) {
+            console.log(err.message)
+            return
+        }
+
+        const book = data[0]
+
+        res.render("editbook", {book})
+    })
+})
+
+// subindo edições
+app.post("/books/updatebook", (req, res) => {
+    const id = req.body.id
+    const title = req.body.title
+    const pages = req.body.pagesqty
+
+    const sql = `UPDATE books SET title = '${title}', pages = ${pages} WHERE idbooks = ${id}`
+
+    conn.query(sql, function(err) {
+        if(err) {
+            console.log(err.message)
+            return
+        }
+
+        res.redirect("/books")
+    })
+})
+// setando configurações para conexão
 const conn = mysql.createConnection({
     host: "localhost",
     user:"root",
@@ -73,6 +121,7 @@ const conn = mysql.createConnection({
     database: "nodemysql"
 })
 
+// conectando e abrindo servidor
 conn.connect(function(err) {
     if(err) {
         console.log(err.message)
---
+++ 8_node_mysql/7_connnection_pool/index.js
@@ -0,0 +1,133 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const pool = require("./db/conn")
+
+
+const app = express()
+
+// transformando dados recebidos em json
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+// setanndo handlebars como template engine
+app.engine("handlebars", exphbs.engine())
+app.set("view engine", "handlebars")
+
+// setado static de css
+app.use(express.static("public"))
+
+// página principal
+app.get("/", (req, res) => {
+    res.render("home")
+})
+
+// criação e postagem de livro no banco de dados
+// a action do formulário será  a mesma action do post
+// ao final, redirecione o usuário para a página pricipal.
+app.post("/books/insertbook", (req, res) => {
+    const title = req.body.title
+    const pages = req.body.pagesqty
+
+    const sql = `INSERT INTO books (title, pages) VALUES ('${title}', '${pages}')`
+
+    pool.query(sql, function(err) {
+        if(err) {
+            console.log(err.message)
+            return
+        }
+        res.redirect("/")
+    })
+})
+
+// conseguindo dados do banco de dados
+app.get("/books", (req, res) => {
+
+    const sql = "SELECT * FROM books"
+
+    pool.query(sql, function(err, data) {
+        if(err) {
+            console.log(err.message)
+            return
+        }
+
+        const books = data
+        res.render("books", {books})
+    })
+})
+
+// selecionando um dado específico com o id do banco de dados
+app.get("/books/:id", (req, res) => {
+    const id = req.params.id
+
+    const sql = `SELECT * FROM books WHERE idbooks = ${id}`
+
+    pool.query(sql, function(err, data) {
+        if(err) {
+            console.log(err.message)
+            return
+        }
+
+        const book = data[0]
+
+        res.render("book", {book})
+    })
+})
+
+// criando página de edição dados
+app.get("/books/edit/:id", (req, res) => {
+
+    const id = req.params.id
+
+    const sql = `SELECT * FROM books WHERE idbooks = ${id}`
+
+    pool.query(sql, function(err, data) {
+        if(err) {
+            console.log(err.message)
+            return
+        }
+
+        const book = data[0]
+
+        res.render("editbook", {book})
+    })
+})
+
+// subindo edições
+app.post("/books/updatebook", (req, res) => {
+    const id = req.body.id
+    const title = req.body.title
+    const pages = req.body.pagesqty
+
+    const sql = `UPDATE books SET title = '${title}', pages = ${pages} WHERE idbooks = ${id}`
+
+    pool.query(sql, function(err) {
+        if(err) {
+            console.log(err.message)
+            return
+        }
+
+        res.redirect("/books")
+    })
+})
+app.post("/books/remove/:id", (req, res) => {
+    const id =  req.params.id
+
+    const sql = `DELETE FROM books WHERE idbooks = ${id}`
+
+    pool.query(sql, function(err) {
+        if(err) {
+            console.log(err.message)
+            return
+        }
+
+        res.redirect("/books")
+    })
+})
+
+// setando configurações para conexão
+app.listen(5000)
